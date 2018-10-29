2072ad50064a81cfe28d86efa8339ad53dae2c4c
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-29 01:16:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     season.each do |day, value| 
-      puts "#{day}"
+      puts "#{day}:"
     end 
   end 
 end
