d97c9dd2f03544e7c7ec2e159061bcabe1196638
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-29 01:25:54 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,10 +61,11 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holiday_hash[season].each do |day, value| 
-      puts "  #{day.capitalize}: #{value.join(", ")}"
+    holiday_hash[season].each do |day, value|
+      day1 = day.gsub("_"," ").capitalize
+      puts "  #{day1}: #{value.join(", ")}"
     end 
-  end 
+  end string = string.gsub("_"," ").capitalize
 end
 
 def all_holidays_with_bbq(holiday_hash)
